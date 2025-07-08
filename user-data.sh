1dc10b89a1a93162590697f44088be9b11b2a816
rajendraacharya1ls <rajendraacharya1ls@gmail.com>
2025-07-08 13:18:59 +0545

    updated

--- user-data.sh
+++ user-data.sh
@@ -7,16 +7,23 @@ apt-get install -y curl python3
 # Create web root directory
 mkdir -p /var/www/html
 
-# Fetch instance metadata from IMDS
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+# Get IMDSv2 token
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+# Fetch metadata using the token
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+  -s http://169.254.169.254/latest/meta-data/instance-id)
+
+AVAIL_ZONE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+  -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
 # Create HTML file with metadata
 cat <<EOF > /var/www/html/index.html
 <html>
   <head><title>EC2 Metadata</title></head>
   <body>
-    <h1>EC2 Instance Metadata (Ubuntu)</h1>
+    <h1>EC2 Instance Metadata (IMDSv2)</h1>
     <p><strong>Instance ID:</strong> $INSTANCE_ID</p>
     <p><strong>Availability Zone:</strong> $AVAIL_ZONE</p>
   </body>
