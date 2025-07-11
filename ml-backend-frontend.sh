98f4a553694741ed1bd889410449f7e0b11ddb1e
rajendraacharya1ls <rajendraacharya1ls@gmail.com>
2025-07-11 12:22:25 +0545

    final

--- ml-backend-frontend.sh
+++ ml-backend-frontend.sh
@@ -1,2 +1,2 @@
 source env/bin/activate
-sudo nohup streamlit run app.py --server.port=8000 --server.headless=true --server.address=0.0.0.0 > streamlit.log 2>&1 &
+nohup streamlit run app.py > streamlit.log 2>&1 &
